bf222f386e7b7f33669eb8bf53a10d8a62284e70
siiikooo0743 <siiikooo0743@gmail.com>
2013-12-21 14:54:29 +0100

    license stuff

--- src/siiikooo0743/jaoum/JAOUM.java
+++ src/siiikooo0743/jaoum/JAOUM.java
@@ -16,6 +16,11 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 
+//JAOUM - Just an other Utlitymod 
+//Copyright (C) 2013 siiikooo0743
+//This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 @Mod(modid = "JAOUM", name = "Just an other Utilitymod", version= "1.0.0")
 @NetworkMod(clientSideRequired = true)
